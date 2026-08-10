87b2705942e8e48ea881a7568c7bc546df4eee36
wdekock <37948573+wdekock@users.noreply.github.com>
2026-08-10 19:44:45 +0200

    Add index.ts to export schema modules

---
+++ packages/meta-core/src/schemas/index.ts
@@ -0,0 +1,6 @@
+// packages/meta-core/src/schemas/index.ts
+import ENTITY_SPEC_SCHEMA from "../../../../Spec-Schema/ENTITY_SPEC_SCHEMA.json" assert { type: "json" };
+import UI_SPEC_SCHEMA from "../../../../Spec-Schema/UI_SPEC_SCHEMA.json" assert { type: "json" };
+import QUESTIONNAIRE_SPEC_SCHEMA from "../../../../Spec-Schema/QUESTIONNAIRE_SPEC_SCHEMA.json" assert { type: "json" };
+
+export { ENTITY_SPEC_SCHEMA, UI_SPEC_SCHEMA, QUESTIONNAIRE_SPEC_SCHEMA };
