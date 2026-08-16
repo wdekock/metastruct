38af8414540a1b4f89d01f4f9e514694e1921e61
wdekock <37948573+wdekock@users.noreply.github.com>
2026-08-16 19:40:00 +0200

    Export modules from index.ts

---
+++ packages/core/src/index.ts
@@ -0,0 +1,8 @@
+export * from './laws/types.js';
+export * from './laws/LawViolationError.js';
+
+export * from './validators/MetaCoreValidator.js';
+export * from './validators/structuralValidator.js';
+export * from './validators/entityLawEvaluator.js';
+export * from './validators/uiLawEvaluator.js';
+export * from './validators/qLawEvaluator.js';
